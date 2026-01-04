ab4fa2348a33c2e8a48bb37d7ae9ede5d8952eaa
Tejen Shrestha <shrestha.tejen@gmail.com>
2026-01-03 19:26:50 -0700

    update base path in vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/nawa',
+    base: '/condies',
     server: {
         port: 3000
     },
